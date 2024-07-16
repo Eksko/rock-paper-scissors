0ba9cb93c5730ad748dcbd217c4ca5d252f48b53
esko <esko.voobus@protonmail.com>
2024-07-16 18:03:17 +0300

    feat: implement round logic & track score

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,3 +1,6 @@
+let computerScore = 0
+let humanScore = 0
+
 function getComputerChoice() {
 	const min = 1
 	const max = 9
@@ -27,4 +30,26 @@ function getComputerChoice() {
 
 function getHumanChoice() {
 	return prompt("Write one: rock, paper or scissors")
+}
+
+function playRound(humanChoice = humanChoice.toLowerCase(), computerChoice) {
+	const rock = "rock"
+	const paper = "paper"
+	const scissors = "scissors"
+
+	if (humanChoice === computerChoice) {
+		console.log("Tie!")
+	} else if (humanChoice === rock && computerChoice === scissors) {
+		humanScore++
+		console.log("You won! Rock beats scissors.")
+	} else if (humanChoice === paper && computerChoice === rock) {
+		humanScore++
+		console.log("You won! Paper beats rock.")
+	} else if (humanChoice === scissors && computerChoice === paper) {
+		humanScore++
+		console.log("You won! Paper beats rock.")
+	} else {
+		computerScore++
+		console.log("Computer won!")
+	}
 }
