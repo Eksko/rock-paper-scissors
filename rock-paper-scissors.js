10d7bd48f1c0f40a78c239c4f3bc84922df7f681
esko <esko.voobus@protonmail.com>
2024-07-16 17:16:18 +0300

    feat: generate & prompt choice
    Generate random choice for computer and prompt for input from user.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,1 +1,30 @@
-console.log("Hello there!")
+function getComputerChoice() {
+	const min = 1
+	const max = 9
+	const num = Math.floor(Math.random() * (max - min + 1)) + min
+
+	let choice = ""
+
+	switch (num) {
+		case 1:
+		case 2:
+		case 3:
+			choice = "rock"
+			break
+		case 4:
+		case 5:
+		case 6:
+			choice = "paper"
+			break
+		case 7:
+		case 8:
+		case 9:
+			choice = "scissors"
+			break
+	}
+	return choice
+}
+
+function getHumanChoice() {
+	return prompt("Write one: rock, paper or scissors")
+}
